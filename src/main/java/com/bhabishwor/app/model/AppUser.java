e6c906fa93fb1ae20d7ba26db74c609bcb410911
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-05-28 20:44:18 +0545

    Add AppUser model

---
+++ src/main/java/com/bhabishwor/app/model/AppUser.java
@@ -0,0 +1,23 @@
+package com.bhabishwor.app.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class AppUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String username;
+	private String email;
+	@JsonIgnore
+	private String password;
+}
